7858634d7acf2da7ed44cf82c50e220ade5d6985
Advit <77945990+advitarora135@users.noreply.github.com>
2022-01-16 17:43:39 -0800

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -10,14 +10,39 @@ const firebaseConfig = {
       appId: "1:958165000115:web:05a56180f01af2fb6cf5c6"
     };
     
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    
+// Initialize Firebase 
+firebase.initializeApp(firebaseConfig);
+
+ user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML="Welcome " + user_name + "!";
+
+function addRoom()
+{
+      room_name = document.getElementById("room_name").value;
+
+      firebase.database().ref("/").child(room_name).update({
+            purpose : "adding room name"
+      });
+      localStorage.setItem("room_name", room_name);
+      window.location="kwitter_page.html";
+}
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
-      //Start code
-
-      //End code
       });});}
 getData();
+function getData2() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+      Room_names = childKey;
+     });});}
+getData();
+
+console.log("Room Name - " + Room_names);
+row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+Room_names+"</div><hr>";
+document.getElementById("output").innerHTML += row;
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
